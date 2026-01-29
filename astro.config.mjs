763eb625b452d8597ba7fafe1be611fe12a40346
Antoine <arotakerpro@gmail.com>
2026-01-29 17:39:54 +0100

    add doc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,40 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { ion } from "starlight-ion-theme";
+import starlightThemeNova from 'starlight-theme-nova'
+
+
 import starlight from '@astrojs/starlight';
+
+import d2 from 'astro-d2';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+    integrations: [starlight({
+        plugins: [
+            // ion(),
+        ],
+        title: 'ARM 32 Friendly Docs',
+		customCss: [
+			'./src/styles/custom.css',
+		],
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        sidebar: [
+            {
+                label: 'Guides',
+                autogenerate: { directory: 'guides' },
+
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+            {
+                label: 'Commandes',
+                autogenerate: { directory: 'commandes' },
+            },
+        ],
+		}), 
+		d2()
 	],
 });
---
+++ src/content/docs/guides/introduction.md
@@ -0,0 +1,16 @@
+---
+title: Introduction
+description: Pourquoi une documentation ARM 32 ?
+---
+
+# Bienvenue dans la documentation ARM 32
+
+Cette documentation a pour but de fournir des informations claires et accessibles sur l'architecture ARM 32 bits, ses applications, et son écosystème. Que vous soyez un développeur cherchant à comprendre les bases de l'ARM 32 ou un ingénieur expérimenté souhaitant approfondir ses connaissances, cette documentation est conçue pour vous accompagner dans votre parcours.
+
+## Objectifs de la documentation
+- **Clarté** : Fournir des explications simples et compréhensibles sur les concepts clés de l'ARM 32.
+- **Accessibilité** : Rendre l'information facilement accessible à tous les niveaux de compétence.
+- **Praticité** : Offrir des exemples concrets et des guides pratiques pour faciliter l'apprentissage et l'application des connaissances.
+
+## Références
+Cette documentation repose sur l'utilisation VisUAL2 (https://salmanarif.bitbucket.io/visual/supported_instructions.html) pour les simulations et les exemples pratiques. N'hésitez pas à explorer cet outil pour mieux comprendre le fonctionnement de l'architecture ARM 32.
